462675bddb82de6192b12b9f611f468491b481a6
Anya <annagapyuk@gmail.com>
2021-12-17 22:40:44 -0500

    created banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./App.css";
 import NavBar from "./components/navbar.jsx";
+import Banner from "./components/banner.jsx";
 
-const App = () => (
-  <div className="app">
-    <NavBar />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="app">
+        <NavBar></NavBar>
+        <Banner></Banner>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./banner.css";
+
+const Banner = () => (
+  <div className="banner">
+    <h1 className="headline">
+      Are you part of a household or business overproducing glass, plastic, or
+      aluminum recyclables that don’t quite make it to curbside or commercial
+      recycling on schedule?
+    </h1>
+  </div>
+);
+
+export default Banner;
